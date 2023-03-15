6155e7784ad6122330c24d3252ec4476992edb8b
Maciej Kachniarz <mkachniarz1@gmail.com>
2023-03-15 22:15:10 +0100

    tweak script

--- assets/profile.ps1
+++ assets/profile.ps1
@@ -1,14 +1,20 @@
-# posh git
-Import-Module posh-git
-
 # oh-my-posh
 oh-my-posh init pwsh --config "$ENV:POSH_THEMES_PATH\\mkachniarz.omp.json" | Invoke-Expression
 
 # menu complete using TAB instead of CTRL+SPACE
 Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete  
+
 # up&down arrow for history search
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward  
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# dotnet completion
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {  
+    param($commandName, $wordToComplete, $cursorPosition)
+    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
 
 $env:AZ_ENABLED = $true
 $env:POSH_GIT_ENABLED = $true
